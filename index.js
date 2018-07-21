e8ff9e1cab6e7213122b113c604da37afba6bd52
sonphanonline <sonphan.online@gmail.com>
2018-07-21 16:56:10 +0700

    add clients lists

--- index.js
+++ index.js
@@ -2,27 +2,48 @@ const app = require('express')()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
+const clients = []
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', socket => {
-    console.log('a user connected')
+    console.log('a user connected: ' + socket.id)
 
-    socket.on('chat message', msg => {
-        console.log(msg)
-        if(msg.indexOf('1') !== -1){
-            io.emit('chat message', 'server : ' + msg)
-        }
-        else{
-            socket.emit('chat message', 'your message: ' + msg)
-        }
-    })
+    clients.push({ id: socket.id, socket: socket })
+
+    io.emit('clients list', clients.map(x=>x.id))
+
+    // socket.on('chat message', msg => {
+    //     console.log(msg)
+
+    //     toId = msg.substring(0,32);
+
+    //     if (msg.indexOf('1') !== -1) {
+    //         io.emit('chat message', 'server : ' + msg)
+    //     }
+    //     else {
+    //         socket.emit('chat message', 'your message: ' + msg)
+    //     }
+    // })
+
     socket.on('disconnect', () => {
-        console.log('user disconnected')
+        console.log('user disconnected: ' + socket.id)
+        index = 0;
+        for (index = 0; index < clients.length; index++) {
+            console.log(clients[index].id)
+            if (clients[index].id === socket.id) {
+                console.log('removed: ' + socket.id)
+                clients.slice(index,1)
+                break
+            }
+        }
+
+        clients.map(x=>console.log(x.id))
     })
 })
 
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log('The server is running: http://localhost:3000')
 })
